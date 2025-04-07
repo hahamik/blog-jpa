2a893527daf87dd09b0bf0dd1ea09594ed244b63
hahamik <dlapdl0615@gmail.com>
2025-04-07 12:12:41 +0900

    게시글 목록보기 완료

--- src/main/java/shop/mtcoding/blog/board/BoardService.java
+++ src/main/java/shop/mtcoding/blog/board/BoardService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.mtcoding.blog.user.User;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class BoardService {
@@ -14,5 +16,16 @@ public class BoardService {
     @Transactional
     public void 글쓰기(BoardRequest.SaveDTO saveDTO, User sessionuser) {
         boardRepository.save(saveDTO.toEntity(sessionuser));
+//                boardRepository.save(Board.builder()
+//                .title(saveDTO.getTitle())
+//                .content(saveDTO.getContent())
+//                .isPublic(saveDTO.getIsPublic() == null ? true : false)
+//                .user(sessionuser)
+//                .build());
+    }
+
+
+    public List<Board> 글목록보기(Integer userId) {
+        return boardRepository.findAll(userId);
     }
 }
---
+++ src/test/java/shop/mtcoding/blog/board/board/BoardRepositoryTest.java
@@ -0,0 +1,39 @@
+package shop.mtcoding.blog.board.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import shop.mtcoding.blog.board.Board;
+import shop.mtcoding.blog.board.BoardRepository;
+
+import java.util.List;
+
+@Import(BoardRepository.class)// BoardRepository
+@DataJpaTest // EntityManger, PC
+public class BoardRepositoryTest {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    public void findAll_test() {
+        // given
+        Integer userId = 1;
+        // when
+        List<Board> boardList = boardRepository.findAll(userId);
+
+        // Lazy -> Board -> User(id=1)
+        // Eager -> N+1  -> Board조회 -> 연관된 User 유저 수 만큼 주회
+//        // Eager -> Join -> 한방쿼리
+//        System.out.println("--------------------");
+//        boardList.get(0).getUser().getUsername();
+//        System.out.println("--------------------");
+        // eye
+        for (Board board : boardList) {
+            System.out.print(board.getId());
+            System.out.print(board.getTitle());
+            System.out.println();
+        }
+    }
+}
